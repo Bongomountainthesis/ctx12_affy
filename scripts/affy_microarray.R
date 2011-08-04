731ab761603410f23abdd1e26a790bc7b64b3bfe
bongomountain <matthew.burney@kcl.ac.uk>
2011-08-04 15:21:39 +0100

    working scripts

--- scripts/affy_microarray.R
+++ scripts/affy_microarray.R
@@ -54,7 +54,11 @@ data.expresso <- expresso(data,
 			summary.method = "avgdiff"
 			)
 
+#save stuff
+save(data.expresso, file = "results/data_quantile.RData")
+
 #get expression matrix and do some stuff
+
 
 E <- exprs(data.expresso)
 
@@ -94,6 +98,53 @@ ensembl[ensembl=="NA"] = NA
 length(which(is.na(ensembl)))
 
 ensmart <- useMart("ensembl", dataset="mmusculus_gene_ensembl")
+
+filters <- "ensembl_gene_id"
+values <- ensembl[!is.na(ensembl)]
+attributes <- c("ensembl_gene_id", "chromosome_name","start_position", "end_position", "strand", "description")
+ens.anno <- getBM(filters=filters, values=values, attributes=attributes, mart=ensmart)
+rownames(ens.anno)<-ens.anno[,1]
+
+anno <- data.frame(
+                   ID = as.character(ids),
+                   EnsemblID = ensembl,
+                   symbol=symbol,
+                   ens.anno[ensembl,],
+                   stringsAsFactors=F
+              )
+rownames(anno) <- anno[,"ID"]
+
+ebFit$genes = anno 
+
+#BMP4vsCTX12=BMP4-CTX12
+#FBSvsCTX12=FBS-CTX12
+#BMP4vsFBS=BMP4-FBS
+
+f.test<- topTable(ebFit, number=nrow(E))
+rownames(f.test)<-f.test$ID
+f.test<-f.test[order(f.test[,"P.Value"]),]
+write.csv(f.test,"results/f_test.csv",row.names=F)
+
+BMP4vsCTX12 <- topTable(ebFit, coef=1, adjust="BH", number=nrow(E))
+rownames(BMP4vsCTX12) <- BMP4vsCTX12$ID
+BMP4vsCTX12 <- BMP4vsCTX12[order(abs(BMP4vsCTX12[,"logFC"]), decreasing=TRUE),]
+BMP4vsCTX12 <- BMP4vsCTX12[!duplicated(BMP4vsCTX12[,"symbol"]),]
+BMP4vsCTX12 <- BMP4vsCTX12[order(BMP4vsCTX12[,"adj.P.Val"],decreasing=FALSE),]
+write.csv(BMP4vsCTX12,"results/BMP4vsCTX12.csv",row.names=F)
+
+FBSvsCTX12<-topTable(ebFit, coef=2, adjust="BH", number=nrow(E))
+rownames(FBSvsCTX12)<-FBSvsCTX12$ID
+FBSvsCTX12 <- FBSvsCTX12[order(abs(FBSvsCTX12[,"logFC"]), decreasing=TRUE),]
+FBSvsCTX12 <- FBSvsCTX12[!duplicated(FBSvsCTX12[,"symbol"]),]
+FBSvsCTX12 <- FBSvsCTX12[order(FBSvsCTX12[,"adj.P.Val"],decreasing=FALSE),]
+write.csv(FBSvsCTX12,"results/FBSvsCTX12.csv",row.names=F)
+
+BMP4vsFBS <-topTable(ebFit, coef=3, adjust="BH", number=nrow(E))
+rownames(BMP4vsFBS)<-BMP4vsFBS$ID
+BMP4vsFBS <- BMP4vsFBS[order(abs(BMP4vsFBS[,"logFC"]), decreasing=TRUE),]
+BMP4vsFBS <- BMP4vsFBS[!duplicated(BMP4vsFBS[,"symbol"]),]
+BMP4vsFBS <- BMP4vsFBS[order(BMP4vsFBS[,"adj.P.Val"],decreasing=FALSE),]
+write.csv(BMP4vsFBS,"results/BMP4vsFBS.csv",row.names=F)
 
 
 
